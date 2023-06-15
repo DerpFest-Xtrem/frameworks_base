cf1627634bd3ab06d9fead9d597bdc7a4c28c886
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-06-15 20:45:04 +0530

    base: Introduce Smart 5G service [1/2]
    Current functionality:
    - Disable 5G in battery saver mode
    - Disable 5G when not on mobile data (such as wifi)
    - Disable 5G on non-active data sim
    
    Not smart enough yet, but we'll get there.
    
    Change-Id: Iac71c1de51429e2287162bb9e45ff4aba3bd0ac9
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6731,6 +6731,12 @@ public final class Settings {
          */
         public static final String QS_DUAL_TONE = "qs_dual_tone";
 
+        /**
+	 * Whether to enable smart 5G mode
+	 * @hide
+	 */
+	public static final String SMART_5G = "smart_5g";
+
 	/**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
